ce312bab723763375c37e8cb125c4a5b555c039f
tiancwyu <tiancwyu@qq.com>
2018-02-26 23:09:04 +0800

    Update spiderJD.py

--- spiderJD.py
+++ spiderJD.py
@@ -17,10 +17,13 @@ class spiderJD(object):
     }
     PROXIES  = {'http' : 'http://fxsl01872:qazwsx123@13.187.24.60:8000/'}
     TIME_OUT = 1000
+    PAT1 = re.compile(r'.*?commentCount":(.*?)\,.*?referenceName":"(.*?)"', re.S)
+    PAT2 = re.compile(r'class="item  selected.*?data-value="(.*?)"', re.S)
+    PATD = re.compile(r'nickname":"(.*?)".*?productColor":"(.*?)"', re.S)
 
     def getContent(self, url):
         try:
-            r = requests.get(url= url, headers= self.HEADER, proxies= self.PROXIES, timeout= self.TIME_OUT)
+            r = requests.get(url= url, headers= self.HEADER, timeout= self.TIME_OUT)
             r.encoding = chardet.detect(r.content)['encoding']
             if not r.ok:
                 print(r.status_code)
@@ -35,10 +38,19 @@ class spiderJD(object):
     def parseNum(self, idP):
         comment_url = "http://club.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv242544&productId=" + idP + "&score=0&sortType=5&page=0&pageSize=10&isShadowSku=0&fold=1"
         raw = self.getContent(comment_url)
-        pattern = re.compile(r'.*?commentCount":(.*?)\,.*?referenceName":"(.*?)"', re.S)
-        data = re.findall(pattern, raw)
+        data = re.findall(self.PAT1, raw)
+        url2 = "http://item.jd.com/" + idP + ".html"
+        raw2 = self.getContent(url2)
+        data2 = re.findall(self.PAT2, raw2)
+        print(data2)
+        if(data2 == []):
+            rate = 500
+        else:
+            rate = self.getNumDetil(idP, data2[0])
+            if(rate > 500):
+                rate = 500
         try:
-            strNum = data[0][1] + ':' + data[0][0]
+            strNum = data[0][1] + ': ' + data[0][0] + ': '+ str(rate) + ': ' + str(int(data[0][0])*(rate/500))
             return strNum
         except Exception as e:
             print(idP)
@@ -49,14 +61,13 @@ class spiderJD(object):
         """为节约时间、流量，仅判断评论数>50000的商品并仅分析前50页"""
         # fo = open("foo1.txt", "w", encoding='utf-8')
         rate = 0
-        patD = re.compile(r'.*?nickname":"(.*?)".*?productColor":"(.*?)"', re.S)
         # patD = re.compile(r'.*?nickname":"(.{0,20})".{0,30}productColor":"(.{0,40})"', re.S)
         for i in range(0,51):
             if((i%10) == 0):
                 print('Current Progress: %d' %(i))
             comment_url = "http://club.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv242544&productId=" + idD + "&score=0&sortType=5&page=" + str(i) + "&pageSize=10&isShadowSku=0&fold=1"
             resD = self.getContent(comment_url)
-            numD = re.findall(patD, resD)
+            numD = re.findall(self.PATD, resD)
             # try:
             #     for m in numD:
             #         numS = m[0] + ':' + m[1]
@@ -72,7 +83,8 @@ class spiderJD(object):
             except Exception as e:
                 pass
         # fo.close()
-        print('Work Done, Totle num = %d' %(rate))
+        return rate
+        # print('Work Done, Totle num = %d' %(rate))
 
 
     def getUrl(self, url):
@@ -81,16 +93,15 @@ class spiderJD(object):
         maxPage = re.findall(patMaxPage, resPage)
         patId = re.compile(r'.*?j-sku-item" data-sku="(.*?)"', re.S)
         id1 = re.findall(patId, resPage)
-        if(maxPage != []):
-            for x in range(2, (int(maxPage[0][2]) + 1)):
-                strUrl = "http://list.jd.com" + maxPage[0][0] + str(x) + maxPage[0][1]
-                print(strUrl)
-                resId = self.getContent(strUrl)
-                id1 = id1 + re.findall(patId, resId)
-            id2 = list(set(id1))
-            return id2
-        else:
-            return id1
+        # if(maxPage != []):
+        # for x in range(2, (int(maxPage[0][2]) + 1)):
+        # strUrl = "http://list.jd.com" + maxPage[0][0] + '2' + maxPage[0][1]
+        # resId = self.getContent(strUrl)
+        # id1 = id1 + re.findall(patId, resId)
+        id2 = list(set(id1))
+        return id2
+        # else:
+            # return id1
 
     def run(self, brand, urlPage):
         fo = open("foo.txt", "w", encoding='utf-8')
@@ -103,7 +114,8 @@ class spiderJD(object):
                     try:
                         i = num.index(b)
                         numL = list(num)
-                        numL.insert(i+len(b),':')
+                        numS = ':' + id +' :'
+                        numL.insert(i+len(b),numS)
                         num = ''.join(numL)
                         flag = 1
                     except Exception as e:
@@ -124,7 +136,5 @@ class spiderJD(object):
 
 if __name__ == '__main__':
     a = spiderJD();
-    # list22 = ['腾达']
-    # a.run(list22, 'http://list.jd.com/list.html?cat=670,592,700&ev=exbrand_16790&page=1&delivery=1&sort=sort_totalsales15_desc&trans=1&JL=4_10_0#J_main')
-    a.getNumDetil('4574935', '【金属机身】1200M')
- 
+    list22 = ['TP-LINK']
+    a.run(list22, 'http://list.jd.com/list.html?cat=670,592,700&ev=exbrand_14073&delivery=1&delivery_211=0&sort=sort_totalsales15_desc&trans=1&JL=2_1_0#J_crumbsBar')
